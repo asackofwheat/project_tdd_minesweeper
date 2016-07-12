da13b54b6624d2f100e17e09ef267c795ebf3457
David Watts <davidjeremywatts@gmail.com>
2016-07-12 15:25:32 -0700

    add tests to board, player

--- spec/minesweeper_spec.rb
+++ spec/minesweeper_spec.rb
@@ -26,7 +26,22 @@ end
 describe 'Player' do
 
   let(:p){Player.new}
+  describe '#initalize' do
+    it 'creates player object'
+    it 'creates player name variable'
+  end
 
+  describe '#get_move_type' do
+    it 'gets users move type choice'
+
+
+  end
+
+  describe '#get_move_loc' do
+    it 'gets users move location choice'
+
+    it 'returns error location as already been triggered by other move type'
+  end
 end
 
 
@@ -37,7 +52,24 @@ describe 'Board' do
 
   describe '#initialize' do
 
-    it 'should create an empty board'
+    it 'should create a bomb grid'
+    it 'should create flag-activate grid'
 
+  end
+
+  describe '#populate' do
+    it 'populates bomb grid with bombs'
+  end
+
+  describe '#number' do
+    it 'fills out bomb grid with proximity numbers'
+  end
+
+  describe '#receive_flag' do
+    it 'adds flag to flag_array'
+  end
+
+  describe '#activate_cell' do
+    it 'adds activation mark to flag-activate grid'
   end
 end
